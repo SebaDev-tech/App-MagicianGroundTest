38be92dd6fe7c3f6b3b1a2151fe67a9680489d47
Sahira Morena Abigail Godoy <210292926+SebaDev-tech@users.noreply.github.com>
2025-10-02 13:36:43 -0300

    a

--- flutter_main/lib/bluetooth_page.dart
+++ flutter_main/lib/bluetooth_page.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Bluetooth Conexión Cimple',
+      title: 'Bluetooth Conexión Simple',
       theme: ThemeData(primarySwatch: Colors.green),
       home: const BluetoothPage(),
     );
